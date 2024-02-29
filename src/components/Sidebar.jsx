1edb9434fa7ba0c200d1118a3b3acf4648ea3cdd
MattCS5 <145207327+MattCS5@users.noreply.github.com>
2024-02-29 19:28:18 +0100

    Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { BadgeCheck, MenuSquare, UserRound, WalletCards } from "lucide-react";
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col bg-black text-white w-56 h-screen gap-2 p-3">
+      <Link className="flex gap-3" href="/">
+        <div className="bg-orange-500 rounded-full text-white p-6">
+          Examen final
+        </div>
+      </Link>
+      <div className="flex flex-col bg-black text-white  h-screen gap-4 p-3">
+        <Link className="flex gap-3" href="/cardGenerator">
+          <MenuSquare />
+          <p>Card Generator</p>
+        </Link>
+        <Link className="flex gap-3" href="/digitalClock">
+          <UserRound />
+          <p>Digital Clock</p>
+        </Link>
+        <Link className="flex gap-3" href="/drillinInfo">
+          <BadgeCheck />
+          <p>Drilling Info</p>
+        </Link>
+        <Link className="flex gap-3" href="/">
+          <WalletCards />
+          <p>Galeria de cartas</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
